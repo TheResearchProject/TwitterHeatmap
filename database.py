cb070a28109582bc8f48c48e565a105a3a601607
Sidney Son <psndnb@gmail.com>
2019-04-22 17:10:33 -0700

    Reorganized Files

--- database.py
+++
@@ -1,7 +0,0 @@
-import pymongo
-
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-
-tdb = myclient["twitterdb"]
-MONGO_HOST = 'mongodb://localhost/twitterdb'
-print(myclient.list_database_names())
